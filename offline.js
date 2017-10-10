4ef3ce6dc560c9a842857cfaa3e2ffacf70fd281
Digoritos <rmc.morelli@gmail.com>
2017-10-10 15:03:36 -0300

    Primeiro protótipo

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1507658655,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/peixoto-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/touchcontrol-sheet0.png",
+		"images/touchcontrol-sheet1.png",
+		"images/touchcontrol-sheet2.png",
+		"images/tiro-sheet0.png",
+		"images/lavaspawner-sheet0.png",
+		"images/lava-sheet0.png",
+		"images/gemaspawner-sheet0.png",
+		"images/gema-sheet0.png",
+		"images/gema-sheet1.png",
+		"images/gema-sheet2.png",
+		"images/greenshield-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
